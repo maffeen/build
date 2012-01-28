a3e9b8322d8484a14843c92dd29f9e552ad1626d
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-28 09:05:16 +0700

    build: Disable dataroaming by default
    The property is removed so that the property in vendor/magma
    can take effect.
    
    Change-Id: Iefadc1a6e71cbf92ecb7363c953f4b5a82064446

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -20,8 +20,7 @@
 # entirely appropriate to inherit from for on-device configurations.
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=true
+    keyguard.no_require_sim=true
 
 ifeq ($(MAGMA_BUILD),)
 PRODUCT_COPY_FILES += \
