e063516d19f2924b09a92550bda2ee07947f3269
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-25 11:21:46 +0300

    build: Bring back rom info on flashing
    @ender1324:
    - add is_gapps function
    - add fluid version

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -804,6 +804,29 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+
+  is_gapps = target_info.GetBuildProp("ro.fluid.version").endswith("-gapped")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  fluidver = target_info.GetBuildProp("ro.fluid.build.version")
+  build_id = target_info.GetBuildProp("ro.build.id")
+  build_date = target_info.GetBuildProp("ro.fluid.build.date")
+  securitypatch = target_info.GetBuildProp("ro.build.version.security_patch")
+  fluiddevice = target_info.GetBuildProp("ro.fluid.device")
+
+  script.Print("--------------------------------------------");
+  script.Print("                ProjectFluid                ");
+  script.Print("--------------------------------------------");
+  script.Print("Android Version: %s"%(androidver));
+  script.Print("Fluid Version: %s"%(fluidver));
+  script.Print("Build ID: %s"%(build_id));
+  script.Print("Build Date: %s"%(build_date));
+  script.Print("Security Patch: %s"%(securitypatch));
+  script.Print("Device: %s"%(fluiddevice));
+  if is_gapps:
+    script.Print("GApps Build: Yes");
+  else:
+    script.Print("GApps Build: No");
+
   device_specific.FullOTA_InstallBegin()
 
   # All other partitions as well as the data wipe use 10% of the progress, and
